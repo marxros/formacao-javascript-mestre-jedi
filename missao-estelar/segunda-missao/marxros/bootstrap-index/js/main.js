5f212631d4068e8648869f710cf83d81948b838d
Marx Roberto <marxmoita@gmail.com>
2018-06-08 10:36:04 -0300

    subindo segunda missao estelar

---
+++ missao-estelar/segunda-missao/marxros/bootstrap-index/js/main.js
@@ -0,0 +1,29 @@
+// Segunda Missão Estelar JS
+
+/*
+
+Apertem os cintos para adentrarmos ao hiper-espaço rumo a uma nova Missão Estelar JS!:)
+
+*/
+
+var missao = "Segunda Missão Estelar JS";
+console.log(missao);
+console.log("Tipo: ", typeof missao);
+
+var hiperespaco = new String("Apertem os cintos para adentrarmos ao hiper-espaço rumo a uma nova Missão Estelar JS!:)");
+
+console.log(hiperespaco);
+console.log("Tipo: ", typeof hiperespaco);
+
+var nomeCliente = "João";
+var renda = 3000;
+var dataNascimento = '1980-02-20';
+var ativo = true ? "sim" : "não";
+
+function exibirDadosClienteVariaveis(){
+    console.log(nomeCliente);
+    console.log(renda);
+    console.log(dataNascimento);
+    console.log(ativo);
+}
+
